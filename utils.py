f6eb6bb66cdacdeb78fa123c1ad4c13632efbe49
PaulinaMamiaga <228375023+PaulinaMamiaga@users.noreply.github.com>
2025-10-26 18:43:39 +0100

    18:43

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@
 # LIST : actions available to a player
 actions = ['explore', 'examine', 'unlock door', 'navigate', 'restart', 'quit']
 
+<<<<<<< Updated upstream
 # LIST : items
 items = ['couch', 'piano', 'queen bed', 'double bed', 'dresser', "dining table"]
 
@@ -37,8 +38,21 @@ game_state = {
     'inventory': [], # to store found keys
     'time': ["display_clock_countdown"] , #library time for live timer and countdown
 }
+=======
+# LIST : actions available to a player
+actions = ['explore', 'examine', 'unlock door', 'navigate', 'restart', 'quit']
+>>>>>>> Stashed changes
 
+# LIST : items
+items = ['couch', 'piano', 'queen bed', 'double bed', 'dresser', "dining table"]
 
+# LIST : doors
+doors = ['door A', 'door B', 'door C', 'door D']
+
+# LIST : keys
+keys = ['key door A', 'key door B', 'key door C', 'key door D']
+
+<<<<<<< Updated upstream
 
 ##############################################################################################
 ################## FUNCTIONS = ACTIONS  ######################################################
@@ -47,6 +61,38 @@ game_state = {
 
 # 0) player_action
 ### // can still input another action or repeat same one ####
+=======
+# Functions corresponding to actions
+
+
+# define Function : explore(current_space)
+def explore(current_space: str):
+    print(f"You are exploring {current_space}. You see these items:")
+    for item in items:
+        print("-", item)
+    return items
+
+# define Function : examine(current_item)
+def examine(current_item: str):
+    print("Examining items carefully...")
+    return True
+
+# define Function : unlock(current_door, inventory)
+def unlock_door(current_door: str, inventory: list):
+    #player_input != 'play'
+    while current_door != 'quit' and current_door != 'restart': # While Loop
+        door = input("Type the door you want to try unlock : ").lower() # Player inputs space of choice
+        if door in doors:
+            print("Player chose to unlock :",door)
+                if door == 'door A':
+            break
+        else:
+            print("Value Error, choose again!")
+    return door
+
+        
+    return door
+>>>>>>> Stashed changes
 
 # 1) explore
 # // update_item_path(item) 
